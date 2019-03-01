805d7bbea1cd68267d330a4cb0f8ce43ff2c3353
Gabriele Pellegrini <gabriele.pellegrini@feries.it>
2019-03-01 11:30:57 +0100

    opzione loader fullscreen

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,6 +1,22 @@
-import React from 'react'
-export default () => (
-  <div className="loader-wrapper">
-    <img className="loader" src="./img/loader.svg" />
-  </div>
-)
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Loader extends Component {
+  render() {
+    const { isFullScreen } = this.props
+    return (
+      <div className={isFullScreen ? 'loader-wrapper' : ''}>
+        <img className="loader" src="./img/loader.svg" />
+      </div>
+    )
+  }
+}
+Loader.propTypes = {
+  isFullScreen: PropTypes.bool
+}
+
+Loader.defaultProps = {
+  isFullScreen: false
+}
+
+export default Loader
