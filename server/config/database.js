f381108a3c4d6aa0b4c03026649151ec8f70476c
Claudio Quaglia <claudio.quaglia@feries.it>
2019-03-19 11:20:29 +0100

    up

--- server/config/database.js
+++ server/config/database.js
@@ -8,7 +8,8 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  debug: process.env.NODE_ENV === 'development'
+  debug: process.env.NODE_ENV === 'development',
+  insecureAuth: process.env.NODE_ENV === 'development'
 })
 
 // Ping database to check for common exception errors.
