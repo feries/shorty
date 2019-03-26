00d2b71a7d11647b2f79d36f7fa8273ef123fd26
Claudio Quaglia <claudio.quaglia@feries.it>
2019-03-26 17:14:29 +0100

    Ky refactor - refresh token action

--- client/src/actions/token.js
+++ client/src/actions/token.js
@@ -1,13 +1,12 @@
-import axios from 'axios'
-
-import Auth from '../lib/Authentication'
-import { API_V1_ENDPOINT, REFRESH_TOKEN } from '../constants/endpoint'
-
+import { REFRESH_TOKEN } from '../constants/endpoint'
 import {
   REFRESH_TOKEN_START,
   REFRESH_TOKEN_SUCCESS,
   REFRESH_TOKEN_ERROR
 } from '../constants/actions'
+
+import { api } from '../lib/helpers'
+import Auth from '../lib/Authentication'
 
 /**
  * @name refreshToken
@@ -20,29 +19,19 @@ import {
 export const refreshToken = async (dispatch, callback, ...params) => {
   try {
     dispatch({ type: REFRESH_TOKEN_START })
-    const {
-      data: { token, refreshToken },
-      status
-    } = await axios.post(`${API_V1_ENDPOINT}${REFRESH_TOKEN}`, {
-      rwt: Auth.getRwt()
-    })
+    const json = { rwt: Auth.getRwt() }
+    const response = await api.post(REFRESH_TOKEN, { json })
 
-    if (status !== 201) return new Error('Refresh token fail.')
+    const { token, refreshToken } = response.json()
 
-    dispatch(refreshTokenSuccess())
+    if (response.status !== 201) return new Error('Refresh token fail.')
+
+    dispatch({ type: REFRESH_TOKEN_SUCCESS })
     Auth.authenticate(token, refreshToken)
     callback && callback(...params)
-  } catch (error) {
-    dispatch(refreshTokenFail())
+  } catch (exception) {
+    dispatch({ type: REFRESH_TOKEN_ERROR })
     Auth.deauthenticate()
     window.location.assign('/login')
   }
 }
-
-export const refreshTokenFail = () => ({
-  type: REFRESH_TOKEN_ERROR
-})
-
-export const refreshTokenSuccess = () => ({
-  type: REFRESH_TOKEN_SUCCESS
-})
